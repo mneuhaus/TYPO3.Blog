503fcc2871c0ec717d67e5b17c71b64aff16addd
Sebastian Kurfürst <sebastian@typo3.org>
2009-09-15 15:17:45 +0000

    [!!!][~FEATURE] BlogExample: Adjusted Flash messages. Related to #4638.

--- Classes/Controller/BlogController.php
+++ Classes/Controller/BlogController.php
@@ -65,7 +65,7 @@ class BlogController extends \F3\FLOW3\MVC\Controller\ActionController {
 	 */
 	public function createAction(\F3\Blog\Domain\Model\Blog $newBlog) {
 		$this->blogRepository->add($newBlog);
-		$this->pushFlashMessage('Your new blog was created.');
+		$this->flashMessages->add('Your new blog was created.');
 		$this->redirect('index');
 	}
 
@@ -88,7 +88,7 @@ class BlogController extends \F3\FLOW3\MVC\Controller\ActionController {
 	 */
 	public function updateAction(\F3\Blog\Domain\Model\Blog $blog) {
 		$this->blogRepository->update($blog);
-		$this->pushFlashMessage('Your blog has been updated.');
+		$this->flashMessages->add('Your blog has been updated.');
 		$this->redirect('index');
 	}
 
@@ -100,7 +100,7 @@ class BlogController extends \F3\FLOW3\MVC\Controller\ActionController {
 	 */
 	public function deleteAction(\F3\Blog\Domain\Model\Blog $blog) {
 		$this->blogRepository->remove($blog);
-		$this->pushFlashMessage('Your blog has been removed.');
+		$this->flashMessages->add('Your blog has been removed.');
 		$this->redirect('index');
 	}
 
